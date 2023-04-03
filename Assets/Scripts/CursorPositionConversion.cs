3b255960cd5e78aa2fdc411981a2a9f391c5a397
Multifaces83 <phtchaos@hotmail.com>
2023-04-03 13:41:00 +0200

    Earth Rotation

--- Assets/Scripts/CursorPositionConversion.cs
+++ Assets/Scripts/CursorPositionConversion.cs
@@ -6,22 +6,33 @@ public class CursorPositionConversion : MonoBehaviour
 {
     private float _xPosition;
     private float _yPosition;
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        //on click
-        if (Input.GetMouseButtonDown(0))
-        {
-            _xPosition = gameObject.transform.position.x;
-            _yPosition = gameObject.transform.position.y;
-            Debug.Log("X: " + _xPosition + " Y: " + _yPosition);
-        }
+        // Récupérer la position de la souris sur l'écran
+        Vector3 mousePos = Input.mousePosition;
 
+        // Convertir la position de la souris en coordonnées du monde
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        // Convertir la position en coordonnées locales de l'objet
+        Vector3 localPos = transform.InverseTransformPoint(worldPos);
+
+        // Récupérer le rayon de la sphère à partir de son échelle
+        float sphereRadius = Mathf.Max(transform.localScale.x, transform.localScale.y, transform.localScale.z) / 2f;
+
+        // Vérifier si la position locale est à l'intérieur de la sphère
+        if (localPos.magnitude <= sphereRadius)
+        {
+            // Faire quelque chose avec la position récupérée
+            // ...
+            Debug.Log(localPos.x);
+        }
     }
+
 }
